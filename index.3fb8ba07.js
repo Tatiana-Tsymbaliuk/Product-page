a2d408e55522bd5234d2dade45099e911c957fdf
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2024-02-29 15:41:44 +0000

    Deploying to gh-pages from @ Tatiana-Tsymbaliuk/Product-page@9bb6321dc201025ee18ee979f9b8aeb17c89b2f4 🚀

---
+++ index.3fb8ba07.js
@@ -0,0 +1,2 @@
+let l=0;const e=document.querySelectorAll(".slide");function o(){e.forEach((l=>{l.style.display="none"})),e[l].style.display="block"}o();
+//# sourceMappingURL=index.3fb8ba07.js.map
---
+++ index.78bad5a9.js
@@ -0,0 +1,2 @@
+!function(){var l=0,n=document.querySelectorAll(".slide");function e(){n.forEach((function(l){l.style.display="none"})),n[l].style.display="block"}e()}();
+//# sourceMappingURL=index.78bad5a9.js.map
